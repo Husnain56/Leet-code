851cc74d23a2f61af61504d1adcb60ca78d91892
Husnain <husnainhere.2019.5@gmail.com>
2025-07-03 16:30:21 +0500

    better-soultion

--- Arrays/Leetcode_13.cpp
+++ Arrays/Leetcode_13.cpp
@@ -13,9 +13,7 @@ int romanToInt(string s)
     for (int i = 0; i < s.length(); i++)
     {
 
-        if ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X')) ||
-            (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C')) ||
-            (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M')))
+        if (map[s[i]]<map[s[i+1]])
         {
             int value1 = map[s[i]];
             int value2 = map[s[i+1]];
